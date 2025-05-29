e902eccaeb6ccda1467a789af19c9d0e7a6b00d5
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-05-29 23:45:42 +0530

    corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
+import conf from './conf/conf.js'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  console.log(conf.appwriteUrl)
   return (
     <>
       <h1>
