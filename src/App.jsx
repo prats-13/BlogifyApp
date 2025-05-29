b2861187a13bf97b2b91d6e73f50e99260c0931f
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-05-29 15:55:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 
 function App() {
-
   return (
     <>
-      <h1>A blog webapp with Appwrite</h1>
+      <h1>
+        A blog webapp with Appwrite
+      </h1>
     </>
   )
 }
