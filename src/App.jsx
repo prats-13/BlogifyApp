de5407d4b2178c50b5775623e708798b9f07b15c
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-06-01 16:03:58 +0530

    configured the app file to get status of login and logout and corrected the main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
+import React, { useState, useEffect } from 'react'
 import './App.css'
-import conf from './conf/conf.js'
+import authService from "./appwrite/auth"
+import {useDispatch} from 'react-redux'
+import {login,logout} from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import {Outlet} from 'react-router-dom'
 
 function App() {
-  console.log(conf.appwriteUrl)
-  return (
-    <>
-      <h1>
-        A blog webapp with Appwrite
-      </h1>
-    </>
-  )
+
+  const [loading,setLoading] = useState(true)
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  }, [])
+
+   return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header>
+          <main>
+           {/*  <Outlet /> */}
+          </main>
+        </Header>
+        <Footer></Footer>
+      </div>
+    </div>
+   ) : null
 }
 
 export default App
