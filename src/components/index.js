8af0ac1f1db5245e51c6be9a7062571f6ae501e2
Pratham Lohia <121216418+prats-13@users.noreply.github.com>
2025-06-09 13:41:30 +0530

    imported button

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-export {Header, Footer, Container, Logo, LogoutBtn}
+import Button from "./Button"
+export {Header, Footer, Container, Logo, LogoutBtn, Button}
